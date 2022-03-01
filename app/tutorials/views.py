fd0deec99eb98ba57d8d9f577ec1bd268b816d49
waltparkman <55926598+waltparkman@users.noreply.github.com>
2022-02-28 20:58:50 -0800

    company name order on index.html

--- app/tutorials/views.py
+++ app/tutorials/views.py
@@ -18,35 +18,44 @@ from rest_framework.decorators import api_view
 
 
 def custList(request):  
-    customers = Customers.objects.all()  
+    print(">>>>>>>>>>>in custList<<<<<<<<<<<<")
+    customers = Customers.objects.all().order_by('company_name')
     return render(request,"tutorials/index.html",{'customers':customers})  
 
 def custCreate(request):  
+    print(">>>>>>>>>>>in custCreate<<<<<<<<<<<<")
     if request.method == "POST":  
         form = CustomerForm(request.POST)  
         if form.is_valid():  
             try:  
                 form.save() 
-                model = form.instance
-                return redirect('tutorials/index.html')  
-            except:  
-                pass  
+                customers = Customers.objects.all().order_by('company_name')
+                return render(request,"tutorials/index.html",{'customers':customers})  
+            except Exception as e: 
+                print(e)
     else:  
         form = CustomerForm()  
     return render(request,'tutorials/cust-create.html',{'form':form})  
 
 def custUpdate(request, id):  
+    print(">>>>>>>>>>>in custUpdate<<<<<<<<<<<<")
     customer = Customers.objects.get(customer_id=id)
     form = CustomerForm(initial={'customer_id': customer.customer_id, 'company_name': customer.company_name, 'contact_name': customer.contact_name, 'contact_title': customer.contact_title, 'address': customer.address, 'city': customer.city, 'region': customer.region, 'postal-code': customer.postal_code, 'country': customer.country, 'phone': customer.phone, 'fax': customer.fax })
     if request.method == "POST":  
+        print(">>>>>>>>>>>in custUpdate check for post <<<<<<<<<<<<")
         form = CustomerForm(request.POST, instance=customer)  
         if form.is_valid():  
+            print(">>>>>>>>>>>in custUpdate form is valid <<<<<<<<<<<<")
             try:  
-                form.save() 
-                model = form.instance
-                return redirect('tutorials/index.html')  
+                print(">>>>>>>>>>>in custUpdate about to save <<<<<<<<<<<<")
+                form.save(commit=True) 
+                print(">>>>>>>>>>>in custUpdate after save <<<<<<<<<<<<")
+                customers = Customers.objects.all().order_by('company_name')
+                return render(request,"tutorials/index.html",{'customers':customers})  
             except Exception as e: 
-                pass    
+                print(e)
+    else:  
+        print(">>>>>>>>>>> request method was not post <<<<<<<<<<")
     return render(request,'tutorials/cust-update.html',{'form':form})  
 
 def custDelete(request, id):
